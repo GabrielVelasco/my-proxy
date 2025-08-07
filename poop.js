1e3b48e0bee1baef7ab41e87574330faf50ad496
GabrielVelasco <themrgabriel100@gmail.com>
2025-08-06 23:18:45 -0300

    bosta

--- poop.js
+++ poop.js
@@ -5,8 +5,7 @@ const app = express();
 
 // Listen on a specific host via the HOST environment variable ()
 var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 // Enable CORS for all routes
 app.use(cors());
