3638dcf22cc94bbc49733e3ee839470aaa0d0529
GabrielVelasco <themrgabriel100@gmail.com>
2025-08-06 23:49:11 -0300

    desistir...

--- poop.js
+++ poop.js
@@ -16,14 +16,7 @@ let browserPromise, reqCount = 0;
     browserPromise = await puppeteer.launch({ 
         headless: true, 
         args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process',
-            '--disable-gpu'
+            '--no-sandbox'
         ]
     });
 })();
